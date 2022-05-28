29ac2d5ab22c8423f6fb7ce48c192060b353f76e
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2022-05-28 23:44:33 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**BY›› [Mᴋɴ Bᴏᴛᴢ™](https://t.me/mkn_bots_updates)**'
+        rep = '**BY›› [ᴅᴍx ʙᴏᴛꜱ™](https://t.me/dmx_bots)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
