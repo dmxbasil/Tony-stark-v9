94a0b35808f3a4027f0ca17877f0bfaa9ab9e17f
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2022-05-29 16:02:12 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -60,7 +60,7 @@ async def showid(client, message):
 @Client.on_message(filters.command("about"))
 async def aboutme(client, message):
         buttons= [[
-            InlineKeyboardButton('📢 UPDATES CHANNEL 📢', url='https://t.me/mkn_bots_updates')
+            InlineKeyboardButton('📢 UPDATES CHANNEL 📢', url='https://t.me/dmx_bots')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
